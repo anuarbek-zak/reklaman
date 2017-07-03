8015f0320001507205a67b2ed834768efa895b2e
Anuarbek Zakiryanov <anuarbekzakirianov97@gmail.com>
2017-07-03 19:38:35 +0600

    a

--- landing/bundles/js/index.js
+++ landing/bundles/js/index.js
@@ -2,17 +2,24 @@
 $( document ).ready(function() {
 
 	var isMobileLoginModalOpen = false;
+	var isMobileMenuModalOpen = false;
 
-		$('#icon-login').click(function() {
-			if(!isMobileLoginModalOpen){
-				$(this).css('backgroundColor','#fff');
-				$('.mobile-login-modal').show();
-				isMobileLoginModalOpen=true	
-			}else{
-				isMobileLoginModalOpen=false;
-				$(this).css('backgroundColor','#eeeeee');
-				$('.mobile-login-modal').hide();
-			}
-			
-		})
+	$("#icon-menu").click(function() {
+		 isMobileMenuModalOpen = showMobileModal($(this),'menu',isMobileMenuModalOpen)
+	})
+	$("#icon-login").click(function() {
+		isMobileLoginModalOpen = showMobileModal($(this),'login',isMobileLoginModalOpen)
+	})
+
+	function showMobileModal(that,val,bool) {	
+		if(!bool){
+			that.css('backgroundColor','#fff');
+			$('.mobile-'+val+'-modal').show();
+			return true;	
+		}else{
+			that.css('backgroundColor','#eeeeee');
+			$('.mobile-'+val+'-modal').hide();
+			return false
+		}
+	}
 });

e6a5b9b5700b84f4c84d60c080324688169fabcb
Anuarbek Zakiryanov <anuarbekzakirianov97@gmail.com>
2017-07-04 00:41:49 +0600

    a

--- landing/bundles/js/index.js
+++ landing/bundles/js/index.js
@@ -7,6 +7,7 @@ $( document ).ready(function() {
 	$("#icon-menu").click(function() {
 		 isMobileMenuModalOpen = showMobileModal($(this),'menu',isMobileMenuModalOpen)
 	})
+
 	$("#icon-login").click(function() {
 		isMobileLoginModalOpen = showMobileModal($(this),'login',isMobileLoginModalOpen)
 	})
@@ -14,10 +15,14 @@ $( document ).ready(function() {
 	function showMobileModal(that,val,bool) {	
 		if(!bool){
 			that.css('backgroundColor','#fff');
+			that.children('.second').show();
+			that.children('.first').hide();
 			$('.mobile-'+val+'-modal').show();
 			return true;	
 		}else{
-			that.css('backgroundColor','#eeeeee');
+			that.css('backgroundColor','transparent');
+			that.children('.second').hide();
+			that.children('.first').show();
 			$('.mobile-'+val+'-modal').hide();
 			return false
 		}
